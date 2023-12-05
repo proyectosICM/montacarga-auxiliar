06de1f602951757c842b047dcab62ce895b00e8f
Eduardo Aguilar <es123y123@gmail.com>
2023-12-05 18:03:04 -0500

    Cam

--- API/urlsApi.jsx
+++ API/urlsApi.jsx
@@ -8,8 +8,8 @@ import { cargarNIP } from "../Hooks/placaLocal";
 //export const base = 'https://montacargas-api-production.up.railway.app/api';
 //export const base = 'http://192.168.1.202:65006/api';
  
-export const base = 'https://samloto.com:65006/api';
-// export const base = '161.132.180.242:65006/api/api'; 
+//export const base = 'https://samloto.com:65006/api';
+export const base = 'https://montacargas-api-production.up.railway.app/api';
 export const carrilesURL = `${base}/carriles`;
 export const asignarMont = `${carrilesURL}/asignarMontacargas/`;
 export const finAuxiliarURL = `${carrilesURL}/finAuxiliar/`
